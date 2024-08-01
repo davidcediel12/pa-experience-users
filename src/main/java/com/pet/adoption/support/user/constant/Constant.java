4f6035128742e380dab647b9f543fa82b4ebf4d3
David Cediel <esdago18@gmail.com>
2024-08-01 15:45:56 +0200

    Add role attribute to the user request

---
+++ src/main/java/com/pet/adoption/support/user/constant/Constant.java
@@ -0,0 +1,8 @@
+package com.pet.adoption.support.user.constant;
+
+public class Constant {
+
+    private Constant(){}
+    public static final String APP_ID = "cb77353b732548a7a6f3ef6eef4401e9";
+    public static final String ROLE_PARAM_NAME = "extension_" + APP_ID + "_Role";
+}
--- src/main/java/com/pet/adoption/support/user/dto/UserRequest.java
+++ src/main/java/com/pet/adoption/support/user/dto/UserRequest.java
@@ -3,10 +3,14 @@ package com.pet.adoption.support.user.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 
+import static com.pet.adoption.support.user.constant.Constant.ROLE_PARAM_NAME;
+
 public record UserRequest(AuthStep step,
                           @JsonProperty("client_id") String clientId,
                           @Email String email,
                           String displayName,
-                          String country) {
+                          String country,
+                          @JsonProperty(ROLE_PARAM_NAME)
+                          String role) {
 
 }
