067b77882b48bf7064a7a45943bb6b165e6d97b8
David Cediel <esdago18@gmail.com>
2024-08-09 13:44:35 +0200

    Handle the exception if the support user API is down

--- src/main/java/com/pet/adoption/business/user/controller/UserControllerAdvice.java
+++ src/main/java/com/pet/adoption/business/user/controller/UserControllerAdvice.java
@@ -3,11 +3,13 @@ package com.pet.adoption.business.user.controller;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.exc.ValueInstantiationException;
 import com.pet.adoption.business.user.dto.Error;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.util.List;
 
@@ -26,7 +28,7 @@ public class UserControllerAdvice {
 
 
         return ResponseEntity.badRequest()
-                .body(new Error("SP-01", "Wrong parameters", errorMessages));
+                .body(new Error("BS-01", "Wrong parameters", errorMessages));
     }
 
 
@@ -37,18 +39,26 @@ public class UserControllerAdvice {
             if (formatException.getTargetType() != null && formatException.getTargetType().isEnum()) {
                 return ResponseEntity
                         .badRequest()
-                        .body(new Error("SP-02", "Wrong step",
+                        .body(new Error("BS-02", "Wrong step",
                                 List.of(formatException.getValue().toString())));
             }
         } else if (e.getCause() instanceof ValueInstantiationException instanceException &&
                 instanceException.getType() != null && instanceException.getType().isEnumType()) {
             return ResponseEntity
                     .badRequest()
-                    .body(new Error("SP-02", "Wrong step",
+                    .body(new Error("BS-02", "Wrong step",
                             List.of(instanceException.getOriginalMessage())));
 
         }
 
         throw e;
+    }
+
+
+    @ExceptionHandler(ResourceAccessException.class)
+    private ResponseEntity<Error> handleResourceAccessException(ResourceAccessException e) {
+
+        var error = new Error("BS-03", "Couldn't obtain the information", List.of(e.getMessage()));
+        return new ResponseEntity<>(error, HttpStatus.BAD_GATEWAY);
     }
 }
