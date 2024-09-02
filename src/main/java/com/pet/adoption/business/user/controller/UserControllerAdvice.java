8084b25a59a4673e8a0f01bbe7f7c6d206db09c2
David Cediel <esdago18@gmail.com>
2024-09-02 20:37:08 +0200

    Use custom exception to manage api calls

--- src/main/java/com/pet/adoption/business/user/controller/UserControllerAdvice.java
+++ src/main/java/com/pet/adoption/business/user/controller/UserControllerAdvice.java
@@ -3,17 +3,22 @@ package com.pet.adoption.business.user.controller;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.exc.ValueInstantiationException;
 import com.pet.adoption.business.user.dto.Error;
+import com.pet.adoption.business.user.exception.ApiCallException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.ResourceAccessException;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestControllerAdvice
+@Slf4j
 public class UserControllerAdvice {
 
 
@@ -60,5 +65,24 @@ public class UserControllerAdvice {
 
         var error = new Error("BS-03", "Couldn't obtain the information", List.of(e.getMessage()));
         return new ResponseEntity<>(error, HttpStatus.BAD_GATEWAY);
+    }
+
+
+    @ExceptionHandler(ApiCallException.class)
+    private ResponseEntity<Error> handleApiCallException(ApiCallException e) {
+
+        HttpClientErrorException cause = (HttpClientErrorException) e.getCause();
+
+        log.error("Error calling API {}, it returns the code {}", e.getCalledApi(), cause.getStatusCode());
+
+        Error serverError = cause.getResponseBodyAs(Error.class);
+        Error clientError;
+        if (serverError != null) {
+            clientError = new Error(serverError.code(), serverError.message(), Collections.emptyList());
+        } else {
+            clientError = new Error("BS-04", "Couldn't obtain the information", Collections.emptyList());
+        }
+
+        return new ResponseEntity<>(clientError, cause.getStatusCode());
     }
 }
--- src/main/java/com/pet/adoption/business/user/service/UserServiceImpl.java
+++ src/main/java/com/pet/adoption/business/user/service/UserServiceImpl.java
@@ -4,11 +4,13 @@ import com.pet.adoption.business.user.client.api.UserApi;
 import com.pet.adoption.business.user.client.model.CreateUserResponse;
 import com.pet.adoption.business.user.client.model.User;
 import com.pet.adoption.business.user.dto.UserRequest;
+import com.pet.adoption.business.user.exception.ApiCallException;
 import com.pet.adoption.business.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.net.URI;
 
@@ -26,10 +28,16 @@ public class UserServiceImpl implements UserService {
         User user = userMapper.toUser(userRequest);
         log.info("Calling user api to save the user");
 
-        ResponseEntity<CreateUserResponse> userResponse = userApi.addUserWithHttpInfo(user);
-        log.info("User saved successfully");
-        log.debug("Response {} END", userResponse.toString());
+        try {
+            ResponseEntity<CreateUserResponse> userResponse = userApi.addUserWithHttpInfo(user);
+            log.info("User saved successfully");
+            log.debug("Response {} END", userResponse.toString());
 
-        return userResponse.getHeaders().getLocation();
+            return userResponse.getHeaders().getLocation();
+
+        } catch (HttpClientErrorException e) {
+            throw new ApiCallException("Support users", e);
+        }
+
     }
 }
