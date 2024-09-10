ab1557fc4de79328791d639da5e3511c8dfb0e24
David Cediel <esdago18@gmail.com>
2024-09-10 11:46:13 +0200

    Ignore unknown properties when deserializing a user request

--- src/main/java/com/pet/adoption/business/user/dto/UserRequest.java
+++ src/main/java/com/pet/adoption/business/user/dto/UserRequest.java
@@ -1,9 +1,11 @@
 package com.pet.adoption.business.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pet.adoption.business.user.constant.Constant;
 import jakarta.validation.constraints.Email;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record UserRequest(AuthStep step,
                           @JsonProperty("client_id") String clientId,
                           @Email String email,
