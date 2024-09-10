6ee0cae22ba80f05ae7fcadc1e41b96ed16d0849
David Cediel <esdago18@gmail.com>
2024-09-10 18:50:58 +0200

    Add expiring time to redis user key

--- src/main/java/com/pet/adoption/business/user/repository/UserRepository.java
+++ src/main/java/com/pet/adoption/business/user/repository/UserRepository.java
@@ -5,9 +5,11 @@ import com.pet.adoption.business.user.dto.UserRequest;
 import com.pet.adoption.business.user.util.JsonSerializer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.time.Duration;
 import java.util.UUID;
 
 @Repository
@@ -17,6 +19,8 @@ public class UserRepository {
 
     private final RedisTemplate<String, String> redisTemplate;
     private final JsonSerializer jsonSerializer;
+    @Value("${spring.data.redis.lifetime-minutes}")
+    private Integer cacheLifeTime;
 
 
     public String saveUser(UserRequest userRequest) {
@@ -25,6 +29,8 @@ public class UserRepository {
 
         redisTemplate.opsForHash().put(userUuid,
                 "user", jsonSerializer.objectToJson(userRequest));
+
+        redisTemplate.expire(userUuid, Duration.ofMinutes(cacheLifeTime));
 
         return userUuid;
     }
