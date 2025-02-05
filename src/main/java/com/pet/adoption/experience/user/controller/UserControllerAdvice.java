9f7a835009723bd3edaeebe2a531ce6b24c3b9a3
David Cediel <esdago18@gmail.com>
2025-02-05 12:08:25 +0100

    Add exception details to error logs

--- src/main/java/com/pet/adoption/experience/user/controller/UserControllerAdvice.java
+++ src/main/java/com/pet/adoption/experience/user/controller/UserControllerAdvice.java
@@ -63,7 +63,7 @@ public class UserControllerAdvice {
     @ExceptionHandler(ResourceAccessException.class)
     private ResponseEntity<Error> handleResourceAccessException(ResourceAccessException e) {
 
-        log.error("Error calling the API {}", e.getMessage());
+        log.error("Error calling the API {}", e.getMessage(), e);
         var error = new Error("BS-03", "Couldn't obtain the information", List.of(e.getMessage()));
         return new ResponseEntity<>(error, HttpStatus.BAD_GATEWAY);
     }
@@ -74,7 +74,7 @@ public class UserControllerAdvice {
 
         HttpClientErrorException cause = (HttpClientErrorException) e.getCause();
 
-        log.error("Error calling API {}, it returns the code {}", e.getCalledApi(), cause.getStatusCode());
+        log.error("Error calling API {}, it returns the code {}", e.getCalledApi(), cause.getStatusCode(), e);
 
         Error serverError = cause.getResponseBodyAs(Error.class);
         Error clientError;
